7d0a7212700926501458098252784c9d6b81c6d8
xiaohui100 <xiaohuihui100@gmail.com>
2017-04-24 15:14:41 +0800

    add worlds

--- first_Python.py
+++ first_Python.py
@@ -2,3 +2,20 @@ print("hell sa")
 i = 4+1
 print(i)
 print("{name} is find {client}".format(name='Alfons',client="dota2"))
+print ("{0:.3}".format(10.0/3))
+print("{0:.^13}".format('hello'))
+print(not 0)
+
+#new line,Function if-else
+number = 5050
+print ("enter number:")
+guss = long(input())
+print (guss)
+
+if guss == number:
+    print("guss == input number")
+    print("hahahah")
+elif guss < number:
+    print ("guss < number")
+else:
+    print ("both shit")
