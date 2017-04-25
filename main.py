991403fee94ccd724364297de3965102f182204d
xiaohui100 <xiaohuihui100@gmail.com>
2017-04-25 12:17:29 +0800

    add Class example

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ print("")
 
 # NEW LINE,Function print_max()
 print("NEW Function----print_max:")
-from Function import print_max, DataStructers
+from Function import print_max
 
 print_max.print_max(4, 5)
 print_max.print_max(5, 4)
@@ -40,11 +40,32 @@ print print_tuple.__doc__
 print('')
 
 # NEW LINE,Function DataStruct
+from Function import DataStructers
 print ("{0:_^64}".format("DataStructers.list_func()"))
 DataStructers.list_func()
-print ("{0:_^64}".format("DataStructers.tuple_func()"))
+print ("\n{0:_^64}".format("DataStructers.tuple_func()"))
 DataStructers.tuple_func()
-print ("{0:_^64}".format("DataStructers.dictionary_func()"))
+print ("\n{0:_^64}".format("DataStructers.dictionary_func()"))
 DataStructers.dictionary_func()
-print ("{0:_^64}".format("DataStructers.seq_function()"))
+print ("\n{0:_^64}".format("DataStructers.seq_function()"))
 DataStructers.seq_function()
+
+# NEW LINE,Class iPhone
+# from iPhone import iPhone
+from iPhone import CiPhone
+print ("\n{0:_^64}".format("class iPhone_v"))
+iPhone_3G = CiPhone.iPhone_v("iPhone_3G")
+iPhone_3G.say_hi()
+CiPhone.iPhone_v.how_many()
+print ""
+
+iPhone_4s = CiPhone.iPhone_v("iPhone_4s")
+iPhone_4s.say_hi()
+CiPhone.iPhone_v.how_many()
+print ""
+
+iPhone_3G.recycle()
+iPhone_4s.recycle()
+CiPhone.iPhone_v.how_many()
+
+
