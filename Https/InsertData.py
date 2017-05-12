29c13b3bca25891887d6863a33859ff5bc465933
xiaohui100 <xiaohuihui100@gmail.com>
2017-05-12 09:34:25 +0800

    add http server

---
+++ Https/InsertData.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python  
+# encoding: utf-8  
+"""
+@author: Alfons
+@contact: alfons_xh@163.com
+@file: InsertData.py 
+@time: 2017/5/11 19:17 
+@version: v1.0 
+"""
+import MySQLdb
+import time
+
+db = MySQLdb.connect(host = "127.0.0.1",
+                     port = 3306,
+                     db = "wifilz",
+                     user = "vm_user",
+                     passwd = "xiaohui",
+                     charset = "utf8")
+cursor = db.cursor()
+import os
+import os.path
+rootdir = "C:\Users\shangchenhui\Desktop\ImportData"  # 指明被遍历的文件夹
+
+files = []
+for parent, dirnames, filenames in os.walk(rootdir):  # 三个参数：分别返回1.父目录 2.所有文件夹名字（不含路径） 3.所有文件名字
+    files = filenames
+
+while True:
+    for file in files:
+        f = open(rootdir + "\\" + file, 'r')
+        for line in f:
+            if line:
+                cursor.execute(line)
+        f.close()
+    db.commit()
+    time.sleep(60)
--- Https/https_server.py
+++ Https/https_server.py
@@ -8,12 +8,19 @@
 @version: v1.0 
 """
 """heldsldf"""
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 import BaseHTTPServer
 import SimpleHTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 import ssl
 
 class S(SimpleHTTPRequestHandler):
+    RecvData = ""
+    RecvNum = 0
     def _set_headers(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
@@ -21,10 +28,22 @@ class S(SimpleHTTPRequestHandler):
 
     def do_POST(self):
         self._set_headers()
-        self.wfile.write("<html><body><h1>POST!</h1></body></html>")
+        length = int(self.headers.getheader('content-length'))
+        data = self.rfile.read(length)
+        if length > 100:
+            self.wfile.write("success!")
+        elif length < 100:
+            self.wfile.write("empty data!")
 
+
+# str_test = "hello"
+# str_1 = str_test.read(3)
+# str_2 = str_test[3:]
+# print str_1
+# print str_2
 
 # httpd = BaseHTTPServer.HTTPServer(('192.168.2.40', 4443), SimpleHTTPServer.SimpleHTTPRequestHandler)
+
 httpd = BaseHTTPServer.HTTPServer(('192.168.2.40', 4443), S)
 httpd.socket = ssl.wrap_socket(httpd.socket, certfile = './server.pem', server_side = True)
 httpd.serve_forever()
