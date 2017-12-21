312b1d8424c098c605abe7602a517794a2fe3206
xiaohui100 <xiaohuihui100@gmail.com>
2017-12-21 12:02:58 +0800

    1、add APK spider

--- WebSpider/APKDownloader/APKDownloadSpider.py
+++ WebSpider/APKDownloader/APKDownloadSpider.py
@@ -18,12 +18,40 @@ apk_check_url_head = "https://apps.evozi.com/apk-downloader/?id="
 apk_download_url = "https://api-apk.evozi.com/download"
 pattern = re.compile(".*?{t:(.*?),(.*?):.*?,(.*?):.*?", re.S)
 
+apk_find_hearder = {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    "connection": "keep-alive",
+    "content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36"
+}
+
+apk_find_cookies = {
+    "logglytrackingsession": "34ca665d-7808-4b2b-834f-921733c6c56b",
+    "__cfduid": "d6f214cc95dab688e156829e2fb8d23431513764291",
+    "evozi_session": "5dulseqdkroephufgs7v44tjq1",
+    "__qca": "P0-1293332314-1513764292003",
+    "_ga": "GA1.2.1413937179.1513764290",
+    "_gid": "GA1.2.1878174561.1513764290"
+}
+
+apk_download_hearder = {
+    "Accept": "application/json, text/javascript, */*; q=0.01",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "zh-CN",
+    "Connection": "	keep-alive",
+    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "Host": "api-apk.evozi.com",
+    "Origin": "https://apps.evozi.com",
+    "referer": "",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0"
+}
+
 def DownloadAPK(soft_dict):
     with open("APK_status.txt", "r") as f:
         soft_list = f.read().split("\n")
 
+    soft_info_dict = {}
     for soft_rank, soft_info_list in soft_dict.items():
-        time.sleep(15)
         try:
             google_url = soft_info_list[-1]
             soft_name = google_url[google_url.find("id=") + 3:]
@@ -31,17 +59,29 @@ def DownloadAPK(soft_dict):
                 continue
 
             apk_check_url = apk_check_url_head + soft_name
-            content_list = requests.get(url = apk_check_url).content.split("\n")
 
+            # 浏览器会话
+            session = requests.session()
+            r = session.get(apk_check_url, headers = apk_find_hearder, timeout = 10)
+            content_list = r.content.split("\n")
+
+            # 获取到的密钥
             code_a = content_list[160].strip()
             keyworld_1 = code_a[code_a.find("'") + 1: code_a.rfind("'")]
 
+            # 获取到的软件名key值代号
             code_b = content_list[187].strip()
             keyworld_list = re.findall(pattern, code_b)[0]
-            post_data = {"t": keyworld_list[0].strip(), keyworld_list[1].strip(): str(soft_name), keyworld_list[2].strip(): keyworld_1, "fetch": "false"}
 
+            # 构造post数据
+            post_data = {
+                "t": keyworld_list[0].strip(),
+                keyworld_list[1].strip(): str(soft_name),
+                keyworld_list[2].strip(): keyworld_1,
+                "fetch": "false"
+            }
             try:
-                reponse = requests.post(url = apk_download_url, data = post_data).content
+                reponse = session.post(url = apk_download_url, headers = apk_find_hearder, data = post_data).content
                 reponse_dict = json.loads(reponse)
             except:
                 with open("except.html", "wb") as f:
@@ -51,16 +91,22 @@ def DownloadAPK(soft_dict):
             if reponse_dict["status"] == "error":
                 continue
 
-            apk_file = requests.get(url = "https:" + reponse_dict["url"]).content
+            apk_file = session.get(url = "https:" + reponse_dict["url"]).content
             with open("Top_100_aps/" + soft_name + ".apk", "wb") as f:
                 f.write(apk_file)
 
             with open("APK_status.txt", "a") as f:
                 f.write(soft_name)
                 f.write("\n")
+
+            soft_info_dict.update({soft_name: reponse_dict})
+
         except:
             traceback.print_exc()
             continue
+    with open("soft_info.json", "a") as f:
+        f.write(json.dumps(soft_info_dict))
+
 
 def dict_slice(adict, start, end):
     keys = adict.keys()
