6ed8fb2bbebc517a0983db8eea760275538229f6
alfonsxh <xiaohuihui100@gmail.com>
2018-07-27 19:35:28 +0800

    1、修改Frida测试代码

--- Frida/WeChatScript/Script/HookLauncherUI.js
+++ Frida/WeChatScript/Script/HookLauncherUI.js
@@ -1,13 +1,35 @@
 Java.perform(function () {
-    var logClass = Java.use("com.tencent.mm.sdk.platformtools.x");
-    logClass.i.overload("java.lang.String", "java.lang.String").implementation = function (str1, str2) {
-       console.log("*************************************");
-       console.log("Original arg: " + str1);
-        // for (var id in str1){
-        //     console("[*]str1: " + id);
-        // }
-        // // console("[*]str1: " + str1.type);
-        // // console("[*]str2: " + str2);
-        // this.i(str1, str2);
-    };
+    // var intentClass = Java.use("com.tencent.mm.plugin.emoji.ui.CustomSmileyPreviewUI");
+    // intentClass.n.implementation = function (customSmileyPreviewUI) {
+    //         console.log("*************************************");
+    //         console.log("Retr_File_Name: " + customSmileyPreviewUI.getIntent().getStringExtra("custom_smiley_preview_md5"));
+    //         console.log("Retr_Msg_Id: " + customSmileyPreviewUI.getIntent().getIntExtra("CropImage_Msg_Id", -1));
+    //         console.log("Retr_Compress_Type: " + customSmileyPreviewUI.getIntent().getIntExtra("CropImage_CompressType", 0));
+    //         console.log("\n");
+    //         return this.n(customSmileyPreviewUI);
+    // }
+
+    // var logClass = Java.use("com.tencent.mm.sdk.platformtools.x");
+    // logClass.i.overload("java.lang.String", "java.lang.String").implementation = function (str1, str2) {
+    //     if ("MicroMsg.LauncherUI" === str1) {
+    //         console.log("*************************************");
+    //         console.log("Original arg1: " + str1);
+    //         console.log("Original arg2: " + str2);
+    //         console.log("\n");
+    //     }
+    //     this.i(str1, str2);
+    // };
+
+    // logClass.i.overload("java.lang.String", "java.lang.String", '[Ljava.lang.Object').implementation = function (str1, str2, str3) {
+    //     if ("MicroMsg.LauncherUI" === str1) {
+    //         console.log("*************************************");
+    //         console.log("Original arg1: " + str1);
+    //         console.log("Original arg2: " + str2);
+    //         console.log("Original arg3: " + str3);
+    //         console.log("\n");
+    //     }
+    //     return this.i(str1, str2, str3);
+    // };
+
+
 });
