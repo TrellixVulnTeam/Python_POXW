b332ade3ef10feb6c6a300a6777004c272b0e292
chenhui.shang <chenhui.shang@woqutech.com>
2019-11-03 14:46:14 +0800

    update

---
+++ LinstorTest/linstor-api-py-1.0.0/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+#=============================================================================
+# FileName: __init__.py.py
+# Desc:
+# Author: chenhui.shang
+# Email: chenhui.shang@woqutech.com
+# HomePage: www.woqutech.com
+# Version: 0.0.1
+# LastChange:  2019/11/3 下午2:43
+# History:
+#=============================================================================
+"""
