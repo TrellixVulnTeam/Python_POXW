6909805c6b195896c20432a8f12c851e9664f8c1
chenhui.shang <chenhui.shang@woqutech.com>
2019-12-23 09:30:35 +0800

    update linstor

--- RedisTest/redisTest.py
+++ RedisTest/redisTest.py
@@ -8,4 +8,5 @@ import redis
 redis_object = redis.Redis(host="192.168.1.52")
 r = redis_object.keys("*")
 redis_object.set("rebalance_after_del_disk_task", 43)
+r_get= redis_object.get("rebalance_aer_del_disk_task")
 pass
