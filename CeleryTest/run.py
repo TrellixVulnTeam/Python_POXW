6fee2d8e4f16567dfe7826f6fe208fcdb282b67e
chenhui.shang <chenhui.shang@woqutech.com>
2020-03-26 10:29:20 +0800

    update linstor

--- CeleryTest/run.py
+++ CeleryTest/run.py
@@ -2,13 +2,20 @@
 # -*- coding: utf-8 -*-
 """
 #=============================================================================
-# FileName: celery_test.py
+# FileName: run.py
 # Desc:
 # Author: chenhui.shang
 # Email: chenhui.shang@woqutech.com
 # HomePage: www.woqutech.com
 # Version: 0.0.1
-# LastChange:  2020/2/21 下午2:26
+# LastChange:  2020/2/25 上午10:48
 # History:
 #=============================================================================
 """
+from CeleryTest import print_test, app
+
+result = print_test.delay()
+pass
+
+
+
---
+++ CeleryTest/tasks.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+#=============================================================================
+# FileName: tasks.py
+# Desc:
+# Author: chenhui.shang
+# Email: chenhui.shang@woqutech.com
+# HomePage: www.woqutech.com
+# Version: 0.0.1
+# LastChange:  2020/2/21 下午2:26
+# History:
+#=============================================================================
+"""
