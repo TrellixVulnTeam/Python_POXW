9efef6d77b2c92bdb82f6d25f6262562bdbbd4e2
chenhui.shang <chenhui.shang@woqutech.com>
2020-06-19 19:16:51 +0800

    update web frameworkers

--- WebFramework/FastAPITest/fastapi_test.py
+++ WebFramework/FastAPITest/fastapi_test.py
@@ -9,7 +9,8 @@
 """
 import time
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -17,7 +18,7 @@ app = FastAPI()
 @app.get("/")
 def index():
     time.sleep(3)
-    return "Hello world"
+    return "Hello ld"
 
 
 @app.get("/items/{item_id}")
@@ -39,4 +40,27 @@ def read_item(item_id: str, q: str = None, short: bool = False):
     return item
 
 
-uvicorn.run(app, host="0.0.0.0", port=8000)
+class Item(BaseModel):
+    name: str
+    description: str = None
+    price: float = None
+    tax: float = None
+
+
+@app.post("/post/{post_id}")
+def post_func(post_id: str, form_1: str = Form(default=None), form_2: str = Form(...)):
+    item = {
+        "post_id": post_id,
+        "form_1": form_1,
+        "form_2": form_2,
+    }
+    # if args1:
+    #     item.update({"args1": args1})
+    # if not short:
+    #     item.update(
+    #         {"description": "This is an amazing item that has a long description"}
+    #     )
+    return item
+
+
+uvicorn.run(app=app, host="0.0.0.0", port=8000)
